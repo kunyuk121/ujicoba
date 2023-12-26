57a58c68b3d6221299e631266918335c60908b31
kunyuk121 <34024261+kunyuk121@users.noreply.github.com>
2023-12-27 02:11:31 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -3,9 +3,3 @@
 while true; do
   # Run your desired command here
   node . drc-20 mint DDMo3czuNP54wr8HB3jbSL8aossS2oGusi like 1000 20
-
-  echo "Sleep for 4 minutes before repeating the command ...."
-  current_time=$(date +"%T")
-  echo "Current time: $current_time"
-  sleep 240
-done
